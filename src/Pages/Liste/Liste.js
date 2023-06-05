5d729025a651c0ee39cd553a74f50a650b6dcfad
djellali <walidbibo1997@gmail.com>
2023-06-05 11:41:46 +0100

    card

--- src/Pages/Liste/Liste.js
+++ src/Pages/Liste/Liste.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import './Liste.css'
+import { useTrail, animated } from 'react-spring'
+import Card from '../../Components/Card/Card'
+import {v4 as uuidv4} from 'uuid'
 export default function Liste() {
+    const trail = useTrail(9, {
+        from: {
+            opacity: 0,
+            x: -20
+        },
+        to: {
+            opacity: 1,
+            x: 0
+        }
+
+    })
   return (
-    <div>
-      Liste
+    <div className='list-container'>
+    {trail.map((card, index)=> {
+        
+        return <animated.div
+        key={uuidv4()}
+        style={card}>
+
+        <Card/>
+        </animated.div>
+    })}
+      
     </div>
   )
 }
